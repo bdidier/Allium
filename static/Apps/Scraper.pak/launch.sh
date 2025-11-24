fef1ccdd2ae37ba95ef6177d76fa733398140ace
Wei Wen Goh <goweiwen@gmail.com>
2025-11-24 20:16:52 +0800

    fix: apps list crash when image is missing

--- static/Apps/Scraper.pak/launch.sh
+++ static/Apps/Scraper.pak/launch.sh
@@ -7,7 +7,9 @@ say "Starting Scraper..."
 if "$ROOT"/.allium/scripts/wait-for-wifi.sh; then
     say "WiFi connected, launching Scraper..."
 
-    "$ROOT/.allium/bin/collie" --roms-path=/mnt/SDCARD/Roms > collie.log
+    touch /tmp/stay_awake
+    "$ROOT/.allium/bin/collie" --bind 0.0.0.0 > collie.log
+    rm /tmp/stay_awake
     exit 0
 fi
 
